eef157fb7de0aba518a64bcb9458a9c1ad8d65ee
Rufus <rufusasterisk@gmail.com>
2017-11-02 11:05:40 -0600

    tune card display html

--- src/components/HouseCard/HouseCard.js
+++ src/components/HouseCard/HouseCard.js
@@ -10,16 +10,10 @@ const HouseCard = (props) => {
       <h2>{props.houseName}</h2>
       <h3>{props.houseWords}</h3>
       <h3>Founded: {props.houseFounded}</h3>
-      <dl>
-        <dt>Seats:</dt>
-        <dd>{props.houseSeats}</dd>
-        <dt>Titles:</dt>
-        <dd>{props.houseTitles}</dd>
-        <dt>Ancestral Weapons:</dt>
-        <dd>{props.houseAncestralWeapons}</dd>
-        <dt>Coat of Arms:</dt>
-        <dd>{props.houseCoatOfArms}</dd>
-      </dl>
+      <h4>Seats: {props.houseSeats}</h4>
+      <h4>Titles: {props.houseTitles}</h4>
+      <h4>Ancestral Weapons: {props.houseAncestralWeapons}</h4>
+      <h4>Coat of Arms: {props.houseCoatOfArms}</h4>
     </article>
   );
 
@@ -36,3 +30,14 @@ HouseCard.propTypes = {
 };
 
 export default HouseCard;
+
+{ /* <dl>
+  <dt>Seats:</dt>
+  <dd>{props.houseSeats}</dd>
+  <dt>Titles:</dt>
+  <dd>{props.houseTitles}</dd>
+  <dt>Ancestral Weapons:</dt>
+  <dd>{props.houseAncestralWeapons}</dd>
+  <dt>Coat of Arms:</dt>
+  <dd>{props.houseCoatOfArms}</dd>
+</dl> */ }
