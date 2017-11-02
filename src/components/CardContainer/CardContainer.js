4393af936f2706d76725e8ab035a4eb1726d30d7
Rufus <rufusasterisk@gmail.com>
2017-11-02 10:38:47 -0600

    Add container template

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import HouseCard from '../HouseCard/HouseCard';
 
 const CardContainer = () => {
 
   const cardArrayBuilder = (myArray) => {
     return myArray.map( (houseObject, index) => {
       return (
-        <article key={`house-${index}`}>
-          <h2>{houseObject.name}</h2>
-          <h3>{houseObject.words || ''}</h3>
-          <h3>Founded: {houseObject.founded || 'N/A'}</h3>
-        </article>
+        <HouseCard
+          key={`house-card-${index}`}
+          houseName={houseObject.name}
+          houseWords={houseObject.words}
+          houseFounded={houseObject.founded || 'N/A'}
+          houseSeats={(houseObject.seats).join(', ')}
+          houseTitles={(houseObject.titles).join(', ')}
+          houseAncestralWeapons={houseObject.ancestralWeapons}
+          houseCoatOfArms={houseObject.coatOfArms}
+        />
       );
     });
   };
 
-  // Display name, founded, seats, titles, coatOfArms, ancestralWeapons, words
+  // Display name, words, founded, seats, titles, ancestralWeapons, coatOfArms
 
   return (
     <section className={'house-card-container'}>
