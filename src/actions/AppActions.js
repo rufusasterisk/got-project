3b3254358bd2fc5c143f516bb8e675a4003c90ef
Rufus <rufusasterisk@gmail.com>
2017-11-02 14:15:40 -0600

    Add code for sworn members API

--- src/actions/AppActions.js
+++ src/actions/AppActions.js
@@ -18,6 +18,14 @@ export const setHouseData = (houseData) => ({
   houseData
 });
 
+const buildFetchPayload = (url) => ({
+  url,
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  method: 'POST'
+});
+
 
 export const getHouseData = (fetchData) => (dispatch) => {
   dispatch(fetchInProgress(true));
@@ -27,6 +35,21 @@ export const getHouseData = (fetchData) => (dispatch) => {
       // console.log(parsedResponse);
       dispatch(fetchInProgress(false));
       dispatch(fetchSuccess(true));
+      const compiledData = parsedResponse.map( (house) => {
+        const unresolvedSwornMemberPromises = house.swornMembers.map( (memberURL) => {
+          fetch('http://localhost:3001/api/v1/character', buildFetchPayload(memberURL))
+            .then( response => response.json())
+            .then(memberObject => memberObject.name);
+        });
+        return Promise.all(unresolvedSwornMemberPromises)
+          .then(swornMemberStringArray => {
+            Object.assign(house, {displayMembers: false, swornMemberNames: swornMemberStringArray});
+          });
+      });
+      compiledData.then(combinedData => {
+        console.log(combinedData);
+
+      })
       dispatch(setHouseData(parsedResponse));
     })
     .catch(error => {
